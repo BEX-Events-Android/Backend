bb2bfbc7f688cdcd48aa886183ead69cc593a3be
Ariana-Matita <arianamatita@gmail.com>
2022-06-10 18:23:38 +0300

    Bex events initial skeleton
    Signed-off-by: Ariana-Matita <arianamatita@gmail.com>

---
+++ src/main/java/com/db/cloud/school/bexevents/BexeventsApplication.java
@@ -0,0 +1,13 @@
+package com.db.cloud.school.bexevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BexeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/cloud/school/bexevents/BexeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.cloud.school.bexevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BexeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
